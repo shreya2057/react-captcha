be22a3cc3c03ea633e7487129a53efefbb9fa975
shreya <shreyashrestha2k57@gmail.com>
2024-07-26 18:12:35 +0545

    feat: login form creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import { Heading, VStack } from "@chakra-ui/react";
-import "./App.css";
+import { Button, Heading, VStack } from "@chakra-ui/react";
+// import "./App.css";
 import { FormControl } from "./component/form/FormControl";
 import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const validationSchema = z.object({
+  email: z.string().email({ message: "Email is invalid" }),
+  password: z.string().min(1, { message: "Password is required" }),
+});
 
 function App() {
-  const form = useForm();
+  const form = useForm<z.infer<typeof validationSchema>>();
   return (
-    <>
-      <VStack>
+    <VStack flex={1} height={"100%"} py={10}>
+      <VStack width={"50%"} as={"form"} gap={6}>
         <Heading>React Captcha</Heading>
         <FormControl
           inputControl="input"
           control={form.control}
-          name="Test"
-          label="test"
+          name="email"
+          label="Email"
         />
+        <FormControl
+          inputControl="input"
+          control={form.control}
+          name="password"
+          label="Password"
+        />
+        <Button>Submit</Button>
       </VStack>
-    </>
+    </VStack>
   );
 }
 
